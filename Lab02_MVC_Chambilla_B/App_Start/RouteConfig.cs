cc840ed44de8c4599326700df70dfd70e1d2f35a
Josue Jimenez <jaechambilla@upt.pe>
2024-09-09 19:42:00 -0500

    second commit

--- Lab02_MVC_Chambilla_B/App_Start/RouteConfig.cs
+++ Lab02_MVC_Chambilla_B/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Lab02_MVC_Chambilla_B
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Persona", action = "MostrarUno", id = UrlParameter.Optional }
             );
         }
     }
